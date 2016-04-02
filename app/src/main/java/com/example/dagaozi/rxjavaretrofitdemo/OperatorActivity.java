e54fcb692ab6cd0332a0600d1e4dc2a658281388
海滨 郝 <dagaozi@163.com>
2016-04-02 14:52:50 +0800

    defer

--- app/src/main/java/com/example/dagaozi/rxjavaretrofitdemo/OperatorActivity.java
+++ app/src/main/java/com/example/dagaozi/rxjavaretrofitdemo/OperatorActivity.java
@@ -2,68 +2,88 @@ package com.example.dagaozi.rxjavaretrofitdemo;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.dagaozi.rxjavaretrofitdemo.Base.BaseActivity;
 import com.example.dagaozi.rxjavaretrofitdemo.Base.BaseSubscriber;
 import com.example.dagaozi.rxjavaretrofitdemo.Base.IBaseSubscriber;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 import butterknife.Bind;
-import butterknife.ButterKnife;
+import butterknife.OnClick;
 import rx.Observable;
+import rx.functions.Func0;
 
 public class OperatorActivity extends BaseActivity implements IBaseSubscriber {
 
     private static final int rang = 1;
     private static final int defer = 2;
     private static final int just = 3;
-    private  StringBuilder result=new StringBuilder() ;
+    @Bind(R.id.btnTest)
+    Button btnTest;
+    private StringBuilder result = new StringBuilder();
     @Bind(R.id.tvResult)
     TextView tvResult;
+    Observable justObservable;
+    Observable deferObservalbe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operator);
-        ButterKnife.bind(this);
-        //rangeTest();
-        deferTest();
+        deferAndJust();
+
     }
 
+    private void deferAndJust() {
+        final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        String time = df.format(new Date());
+         justObservable = Observable.just(df.format(new Date()));
+
+         deferObservalbe = Observable.defer(new Func0<Observable<String>>() {
+             @Override
+             public Observable<String> call() {
+                 return Observable.just(df.format(new Date()));
+             }
+         });
+    }
+
+    @OnClick(R.id.btnTest)
+    public void btnClink(){
+        rangeTest();
+       // deferTest();
+    }
     private void rangeTest() {
         //发射5个不大于等于的数据
         Observable.range(10, 5).subscribe(new BaseSubscriber<Integer>(this, rang));
     }
-    //defer实在订阅那一刻才赋值;just在创建Observable的时候就赋值。
-    private void deferTest(){
-         Object i=8;
-    /*  Observable deferObservalbe=  Observable.defer(new Func0<Observable<Object>>() {
-            @Override
-            public Observable<Object> call() {
-                return Observable.just(i);
-            }
-        });*/
-        Observable justObservable=Observable.just(i);
-       i=12;
-       justObservable.subscribe(new BaseSubscriber<Objects>(this,just));
-      //  deferObservalbe.subscribe(new BaseSubscriber<Objects>(this,defer));
+
+    //defer是在订阅那一刻才赋值;just在创建Observable的时候就赋值。
+    private void deferTest() {
+        //创建justObservable时已经赋值
+        //justObservable.subscribe(new BaseSubscriber<String>(this, just));
+        //这一刻才赋值
+        deferObservalbe.subscribe(new BaseSubscriber<Objects>(this,defer));
     }
 
     @Override
     public void onNext(Object o, int flag) {
         switch (flag) {
             case rang:
-                result.append((Integer)o+" ");
-               tvResult.setText(result.toString());
+                result.append((Integer) o + " ");
+                tvResult.setText(result.toString());
                 Log.d("Rang", ((Integer) o) + "");
                 break;
-            case defer:
-                tvResult.setText((Integer)o+"");
-                break;
             case just:
-                tvResult.setText((Integer)o+"");
+                tvResult.setText((String) o);
+            case defer:
+                tvResult.setText((String) o);
+                break;
+
             default:
                 Log.d("default", "no fand flag");
         }
